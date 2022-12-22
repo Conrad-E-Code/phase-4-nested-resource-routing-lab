5bac5061315620151b49bdbe95b0eb0b35d23e76
Conrad Etherington <conrad.etherington@gmail.com>
2022-12-22 16:01:05 -0700

    commit tree 5dfffb8a8f3d3beeec9b76a33b7fe92307f3902e

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] 
+  resources :users, only: [:show] do
+    resources :items
+  end
+
+  # get "/users/:user_id/items", to: "items#items_by_id"
+
+  # get "/users/:user_id/items/:id", to: "items#show"
+
+  # post "/users/:user_id/items/:id", to: "items#create"
 end
